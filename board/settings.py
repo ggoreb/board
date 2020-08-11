c3a69f05e756da3070875459da8b559451a67c66
ggoreb <seorab@naver.com>
2020-08-11 16:43:56 +0900

    settings -> Allowed Hosts 수정

--- board/settings.py
+++ board/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e4ukymm4g%y7-izzz#qqfkgoj0q!eqgoxstlv=ltevj)mk5kpv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
